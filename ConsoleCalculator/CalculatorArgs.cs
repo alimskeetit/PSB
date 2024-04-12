115353e3c94f61cbdc06f8f8a40f0edc687a4ddc
Ivan <afkanovich@yandex.ru>
2024-04-12 23:31:01 +0300

    fix uri encoding for double

--- ConsoleCalculator/CalculatorArgs.cs
+++ ConsoleCalculator/CalculatorArgs.cs
@@ -1,27 +1,31 @@
+using System.Globalization;
 using Microsoft.Extensions.Configuration;
 using PowerArgs;
+using System.Globalization;
 
 namespace ConsoleCalculator;
+
 [ArgExceptionBehavior(ArgExceptionPolicy.StandardExceptionHandling)]
 public class CalculatorArgs
 {
     private readonly HttpClient _client;
-    private readonly string _baseAddress;
     
     public CalculatorArgs()
     {
+       
         _client = new HttpClient();
         var config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
             .Build();
-        _baseAddress = config.GetRequiredSection("calculatorUrl").Value!;
+        _client.BaseAddress = new Uri(config.GetRequiredSection("calculatorUrl").Value!);
     }
     
     [ArgActionMethod]
     [ArgDescription("Sums up two numbers")]
     public async Task Sum(double firstNumber, double secondNumber)
     {
-        var response = await _client.GetAsync($"{_baseAddress}/sum?firstNumber={firstNumber}&secondNumber={secondNumber}");
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        var response = await _client.GetAsync($"sum?firstNumber={firstNumber}&secondNumber={secondNumber}");
         Console.WriteLine(await response.Content.ReadAsStringAsync());
     }
     
@@ -29,7 +33,8 @@ public class CalculatorArgs
     [ArgDescription("Subtracts two numbers")]
     public async Task Subtraction(double firstNumber, double secondNumber)
     {
-        var response = await _client.GetAsync($"{_baseAddress}/subtraction?firstNumber={firstNumber}&secondNumber={secondNumber}");
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        var response = await _client.GetAsync($"subtraction?firstNumber={firstNumber}&secondNumber={secondNumber}");
         Console.WriteLine(await response.Content.ReadAsStringAsync());
     }
     
@@ -37,7 +42,8 @@ public class CalculatorArgs
     [ArgDescription("Multiplies two numbers")]
     public async Task Multiplication(double firstNumber, double secondNumber)
     {
-        var response = await _client.GetAsync($"{_baseAddress}/multiplication?firstNumber={firstNumber}&secondNumber={secondNumber}");
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        var response = await _client.GetAsync($"multiplication?firstNumber={firstNumber}&secondNumber={secondNumber}");
         Console.WriteLine(await response.Content.ReadAsStringAsync());
     }
     
@@ -45,7 +51,8 @@ public class CalculatorArgs
     [ArgDescription("Divides two numbers")]
     public async Task Division(double firstNumber, double secondNumber)
     {
-        var response = await _client.GetAsync($"{_baseAddress}/division?firstNumber={firstNumber}&secondNumber={secondNumber}");
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        var response = await _client.GetAsync($"division?firstNumber={firstNumber}&secondNumber={secondNumber}");
         Console.WriteLine(await response.Content.ReadAsStringAsync());
     }
 }
